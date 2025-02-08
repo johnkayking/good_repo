5928f14729f762f3355dea1ec76e9874cf033ed9
johnkayking <johnkayking@gmail.com>
2025-02-08 13:26:48 +0000

    ny change

---
+++ python.py
@@ -0,0 +1,15 @@
+new = "hello world"
+print(new)
+
+def add(a,b):
+    return a + b
+print(add(4,5))
+
+my_list = [2,4,5,6,8,2,2]
+
+def cal(lst):
+    for i in (lst):
+        print(i)
+
+cal(my_list)
+        
